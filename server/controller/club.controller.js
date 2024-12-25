c221c1d1a86cfc02a74a17dba768a5b2f52abd84
JeevithP <jeevithp171@gmail.com>
2024-12-25 23:58:44 +0530

    Updated routes and controllers for CRUD operations

--- server/controller/club.controller.js
+++ server/controller/club.controller.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import { generateToken } from "../utils/generateToken.js";
-import pool from './connection.js';
+import pool from '../db/connection.js';
 
 export const clubLogin=(req,res)=>{
 
